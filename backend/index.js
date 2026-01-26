6b6a9e7f04d66cd9447b2c0806030f6497a38c47
Ankesh Kumar <2004ankesh@gmail.com>
2026-01-26 15:50:51 +0530

    fixe for railway

--- backend/index.js
+++ backend/index.js
@@ -17,9 +17,11 @@ import { socketHandler } from "./socket.js"
 const app=express()
 const server=http.createServer(app)
 
+const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"];
+
 const io=new Server(server,{
    cors:{
-    origin:"http://localhost:5173",
+    origin: allowedOrigins,
     credentials:true,
     methods:['POST','GET']
 }
@@ -31,7 +33,7 @@ app.set("io",io)
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin: allowedOrigins,
     credentials:true
 }))
 app.use(express.json())
