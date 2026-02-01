ee1216a9b58bf9b832c22391987be16df4cbd730
Ankesh Kumar <2004ankesh@gmail.com>
2026-02-01 15:16:26 +0530

    fixed vite.config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,20 +3,17 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   build: {
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('firebase')) return 'firebase';
-            if (id.includes('leaflet') || id.includes('react-leaflet')) return 'maps';
-            if (id.includes('recharts')) return 'charts';
-            if (id.includes('react') || id.includes('redux')) return 'vendor';
-          }
-        },
-      },
+      // output: {
+      //   manualChunks(id) {
+      //     if (id.includes('node_modules')) {
+      //       return 'vendor';
+      //     }
+      //   },
+      // },
     },
   },
 })
